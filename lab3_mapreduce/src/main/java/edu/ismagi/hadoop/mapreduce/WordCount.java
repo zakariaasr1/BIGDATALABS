82d585c77aee363edf3590c958b2f13dc6df26e0
vboxuser <vboxuser@debain11.myguest.virtualbox.org>
2025-11-30 21:14:57 +0100

    Ajout TP3 - MapReduce (WordCount)

---
+++ lab3_mapreduce/src/main/java/edu/ismagi/hadoop/mapreduce/WordCount.java
@@ -0,0 +1,40 @@
+package edu.ismagi.hadoop.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            System.err.println("Usage: WordCount <input> <output>");
+            System.exit(1);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+
+        // classe principale
+        job.setJarByClass(WordCount.class);
+
+        // map & reduce
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // fichier d'entrée et sortie
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
